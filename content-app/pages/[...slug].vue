83c21a49e83dd4d31cc9876b3d7c5a3ff20dd253
Cody Bontecou <bontecouc@gmail.com>
2025-01-15 19:31:24 -1000

    simplified slug route

--- content-app/pages/[...slug].vue
+++ content-app/pages/[...slug].vue
@@ -1,18 +1,3 @@
-<script setup lang="ts">
-import { generateUniqueKey } from '~/lib/generateRandomKey'
-
-const getPostByPath = async () =>
-    await queryContent()
-        .where({ _path: { $eq: useRoute().path } })
-        .findOne()
-
-const { data: post } = await useAsyncData(generateUniqueKey(), getPostByPath)
-</script>
-
 <template>
-    <main v-if="post">
-        <ContentDoc :path="post._stem">
-            <ContentRenderer :value="post" />
-        </ContentDoc>
-    </main>
+    <ContentDoc />
 </template>
