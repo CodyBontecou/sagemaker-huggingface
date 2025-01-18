d6eca695835f3db9cf2e1b36dfc9460cbf4dc1f7
Cody Bontecou <bontecouc@gmail.com>
2025-01-18 07:09:32 -1000

    removing translate plugin

--- server/plugins/translate.ts
+++ server/plugins/translate.ts
@@ -2,24 +2,22 @@ import { invokeSageMakerEndpoint } from '../utils/invokeSageMakerEndpoint'
 import { handleFileCreation } from '../utils/handleFileCreation'
 
 export default defineNitroPlugin(async nitroApp => {
-    const { AWS_ENDPOINT_NAME, AWS_REGION } = useRuntimeConfig()
-    const lang = {
-        src: 'eng',
-        tgt: 'spa',
-    }
-
-    nitroApp.hooks.hook('content:file:beforeParse', async file => {
-        const response: [{ translation_text: string }] =
-            await invokeSageMakerEndpoint(
-                AWS_ENDPOINT_NAME,
-                AWS_REGION,
-                file.body,
-                lang.src,
-                lang.tgt
-            )
-
-        if (!file._id.includes('content:spa:')) {
-            handleFileCreation(file, response[0].translation_text, lang.tgt)
-        }
-    })
+    // const { AWS_ENDPOINT_NAME, AWS_REGION } = useRuntimeConfig()
+    // const lang = {
+    //     src: 'eng',
+    //     tgt: 'spa',
+    // }
+    // nitroApp.hooks.hook('content:file:beforeParse', async file => {
+    //     const response: [{ translation_text: string }] =
+    //         await invokeSageMakerEndpoint(
+    //             AWS_ENDPOINT_NAME,
+    //             AWS_REGION,
+    //             file.body,
+    //             lang.src,
+    //             lang.tgt
+    //         )
+    //     if (!file._id.includes('content:spa:')) {
+    //         handleFileCreation(file, response[0].translation_text, lang.tgt)
+    //     }
+    // })
 })
