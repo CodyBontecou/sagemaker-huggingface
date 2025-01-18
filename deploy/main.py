fd0f2b9120cc7ff7c2754328b6ba528d0db281a4
Cody Bontecou <bontecouc@gmail.com>
2025-01-18 07:14:20 -1000

    sagemaker endpoint deploy script

---
+++ deploy/main.py
@@ -0,0 +1,26 @@
+import sagemaker
+import boto3
+from sagemaker.huggingface import HuggingFaceModel
+try:
+    role = sagemaker.get_execution_role()
+except ValueError:
+    iam = boto3.client("iam")
+    role = iam.get_role(RoleName="SageMakerExecutionRole")["Role"]["Arn"]
+# Hub Model configuration. https://huggingface.co/models
+hub = {
+    "HF_MODEL_ID": "facebook/seamless-m4t-v2-large",
+    "HF_TASK": "translation",
+}
+# create Hugging Face Model Class
+huggingface_model = HuggingFaceModel(
+    transformers_version="4.37.0",
+    pytorch_version="2.1.0",
+    py_version="py310",
+    env=hub,
+    role=role,
+)
+# deploy model to SageMaker Inference
+predictor = huggingface_model.deploy(
+    initial_instance_count=1,  # number of instances
+    instance_type="ml.g4dn.xlarge",  # ec2 instance type
+)
